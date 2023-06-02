c7edfc6ab03b44ee29c54d35e33167954a68b5a5
josuecaceres <elvinjcaceress@gmail.com>
2023-06-02 00:12:25 -0600

    authentication finish

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+require("dotenv").config();
+
+// DB Config
+require("./src/database/config").dbConnection();
+
+// App de Express
+const app = express();
+
+// Lectura y parseo del Body
+app.use(express.json());
+
+// Node Server
+const server = require("http").createServer(app);
+module.exports.io = require("socket.io")(server);
+require("./src/sockets/socket");
+
+// Path público
+const publicPath = path.resolve(__dirname, "public");
+app.use(express.static(publicPath));
+
+// Mis Rutas
+app.use("/api/login", require("./src/routes/auth"));
+
+server.listen(process.env.PORT, (err) => {
+  if (err) throw new Error(err);
+
+  console.log("Servidor corriendo en puerto", process.env.PORT);
+});
